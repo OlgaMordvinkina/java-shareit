90a9eed5a17219b6bfadcaae0b40ec25e9d82bb0
olga <OlOrlovaS@yandex.ru>
2023-04-17 10:32:36 +0300

    feat: add-controllers ТЗ-13 ревью-1

--- src/main/java/ru/practicum/shareit/item/ItemStorageImpl.java
+++ src/main/java/ru/practicum/shareit/item/ItemStorageImpl.java
@@ -55,9 +55,7 @@ public class ItemStorageImpl implements ItemStorage {
 
     @Override
     public Collection<Item> getItems(Long ownerId) {
-        return items.values().stream()
-                .filter(it -> Objects.equals(it.getOwner(), ownerId))
-                .collect(Collectors.toList());
+        return items.values().stream().filter(it -> Objects.equals(it.getOwner(), ownerId)).collect(Collectors.toList());
     }
 
     @Override
